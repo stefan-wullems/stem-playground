f5ab67cbfc9797fe632f1ffb59c28b3aabc11080
stefanwullems <stefan@exivity.com>
2020-11-23 21:23:15 +0100

    test boolean-expression

--- packages/core/src/domains/boolean-logic/boolean-expression.test.ts
+++ packages/core/src/domains/boolean-logic/boolean-expression.test.ts
@@ -1,6 +1,6 @@
 import { performanceTest } from '../../test-utils/preformance'
 
-import { getVariables } from './boolean-expression'
+import { getVariables, evaluateExpression } from './boolean-expression'
 
 describe('boolean-expression#getVariables', () => {
   it('returns an array with all variables', () => {
@@ -9,12 +9,31 @@ describe('boolean-expression#getVariables', () => {
 
   it('performance', () => {
     performanceTest('getVariables: same expression', () => {
-      getVariables('(A|B)&!D')
+      getVariables('(A&B)|!C')
     }, 3)
 
     const expressions = ['(B|A)|!C', '(B&A)|C', '!(B&A)&C']
     performanceTest('getVariables: different expressions', (index) => {
       getVariables(expressions[index])
+    }, 3)
+  })
+})
+
+describe('boolean-expression#evaluateExpression', () => {
+  it('evaluates the expressions correctly', () => {
+    expect(evaluateExpression('(D|E)&!F', ['D', 'E', 'F'])).toEqual(false)
+    expect(evaluateExpression('(D|E)&!F', ['E', 'F'])).toEqual(false)
+    expect(evaluateExpression('(D|E)&!F', ['E'])).toEqual(true)
+  })
+
+  it('performance', () => {
+    performanceTest('evaluateExpression: same expression', () => {
+      evaluateExpression('(D&E)|!F', ['D', 'E', 'F'])
+    }, 3)
+
+    const expressions = ['(D|E)|!F', '(D&E)|F', '!(D&E)&F']
+    performanceTest('evaluateExpression: different expressions', (index) => {
+      evaluateExpression(expressions[index], ['D', 'E', 'F'])
     }, 3)
   })
 })
